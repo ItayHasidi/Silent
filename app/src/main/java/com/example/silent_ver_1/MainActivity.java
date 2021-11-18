5742b961558c304b7c0fff4caf04a4b6fc6b2cb2
Daniel Okavi <74859686+OkaviDaniel@users.noreply.github.com>
2021-11-18 12:00:52 +0200

    Basic ui design

--- app/src/main/java/com/example/silent_ver_1/MainActivity.java
+++ app/src/main/java/com/example/silent_ver_1/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.silent_ver_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sideBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sideBar = (Button)findViewById(R.id.sideBarBtn);
+    }
+
+    public void openSideMenu(View view) {
+
     }
 }
