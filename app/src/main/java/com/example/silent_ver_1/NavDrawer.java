119b389309d16976f426c664b1aecfa5885124c4
Daniel Okavi <74859686+OkaviDaniel@users.noreply.github.com>
2021-11-28 14:44:40 +0200

    Finish of the sidebar items

--- app/src/main/java/com/example/silent_ver_1/NavDrawer.java
+++ app/src/main/java/com/example/silent_ver_1/NavDrawer.java
@@ -41,7 +41,7 @@ public class NavDrawer extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_settings, R.id.nav_sync_now, R.id.nav_sync_options
+                R.id.nav_home, R.id.nav_settings, R.id.nav_sync_now, R.id.nav_sync_options
         ,R.id.nav_premium,R.id.nav_about)
                 .setOpenableLayout(drawer)
                 .build();
